d7fc5d283e8afcb1f27aed8d41b4c51868130998
tosmel2 <adewaletosin0808@gmail.com>
2021-06-18 05:14:42 -0700

    New 4 puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- *main -  entry point
- *
- *Return: 0 after printing the function
- */
+*main -  entry point
+*
+*Return: 0 after printing the function
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
