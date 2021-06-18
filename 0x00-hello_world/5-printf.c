e2fd72fd7b2e9f724d5e173ea9c35af377dd7c91
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-06-18 20:22:56 +0800

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,14 +1,11 @@
 #include <stdio.h>
-
 /**
-* main - print the string in the put function
-*
-* Description: using the main function
-* this program prints "Programming is like building a multilingual puzzle
-* Return: 0
-*/
+ *main - Entry point
+ *
+ *Return: retun 1 after program execution
+ */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
