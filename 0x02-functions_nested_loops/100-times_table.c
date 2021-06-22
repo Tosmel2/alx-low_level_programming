b7cfe3fc7ac9515db07882b600bee4220b819257
tosmel2 <adewaletosin0808@gmail.com>
2021-06-23 02:38:55 +0800

    New 100

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,11 +1,39 @@
 #include "holberton.h"
 /**
- * print_times_table - Entry point
- * @n: input
- * Return: Always 0 (Success)
+ * times_table -  a function that prints the 9 times table, starting with 0
+ * rone = row, cone = column, d = digits of current result
+ * Return: times table
+ * add extra space past single digit
  */
-void print_times_table(int n)
+void times_table(void)
 {
+	int rone, cone, d;
 
-	return (0);
-}
+	for (rone = 0; rone <= 3; rone++)
+	{
+		_putchar('0');
+		_putchar(',');
+		_putchar(' ');
+		for (cone = 0; cone <= 5; cone++)
+		{
+			d = (rone * cone);
+			if ((d / 98) > 0)
+			{
+				_putchar((d / 10) + '0');
+			}
+			else
+			{
+				_putchar(' ');
+			}
+			_putchar((d % 98) + '0');
+
+			if (cone < 12)
+			{
+				_putchar(',');
+				_putchar(' ');
+			}
+		}
+		_putchar('\n');
+	}
+
+}}
