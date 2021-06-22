2a38548e02eb7d2a43ffc656b7bf1edab2776e8a
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-06-23 01:12:53 +0800

    Update 8-24_hours.c

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,33 +1,25 @@
 #include "holberton.h"
 /**
- * jack_bauer - func that prints every minute of the day of Jack Bauer, starti
- * n from 00:00 to 23:59, min loop counts mins, while hour loop counts hours
- * and resets mins
- * Return: 0
+ * jack_bauer - function that prints every minute of the day, 24 hour clock
+ * h = hour, m = minutes
+ * / 10 allows second digit to rotate
+ * for loop breaks before passing 24:00
+ * Return: 24 hour clock line by line
  */
 void jack_bauer(void)
 {
-	int hours = 0;
-	int minutes = 0;
-	int hours_remainder;
-	int mins_remainder;
+	int h, m;
 
-	while (hours <= 23)
+	for (h = 0; h < 24; h++)
 	{
-	while (minutes <= 59)
-	{
-	mins_remainder = minutes % 10;
-	hours_remainder = hours % 10;
-	_putchar(hours / 10 + '0');
-	_putchar(hours_remainder + '0');
-	_putchar(':');
-	_putchar(minutes / 10 + '0');
-	_putchar(mins_remainder + '0');
-	minutes++;
-	_putchar('\n');
+		for (m = 0; m < 60; m++)
+		{
+			_putchar((h / 10) + '0');
+			_putchar((h % 10) + '0');
+			_putchar(':');
+			_putchar((m / 10) + '0');
+			_putchar((m % 10) + '0');
+			_putchar('\n');
+		}
 	}
-	hours++;
-	minutes = 0;
 }
-}
-
