4ae2786f437418f39b757027408e7678f344b936
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-06-25 03:49:34 +0800

    Update 0-main.c

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
-
 /**
-* main - tests function that prints if integer is positive or negative
+* main - main
 * Return: 0
 */
 
-int 0-main(void)
+int main(void)
 {
-        int i;
 
-        i = 98;
-        positive_or_negative(i);
+int i;
+i = 0;
 
-        return (0);
+positive_or_negative(i);
+
+return (0);
 }
