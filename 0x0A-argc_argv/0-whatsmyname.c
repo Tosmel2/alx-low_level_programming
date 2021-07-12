ae3b43f452ff9ff2ca433d7dbab4b653236e378a
tosmel2 <adewaletosin0808@gmail.com>
2021-07-12 11:37:56 -0700

    whatsmyname

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -7,7 +7,7 @@
  *     *     * @argv: arguments to program
  *      *      *
  *       *       * Return: 0 if successful
- *        *        */
+*/
 int main(int argc, char **argv)
 {
 			printf("%s\n", argv[argc - argc]);
