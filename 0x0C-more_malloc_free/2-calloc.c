b843e534fea178f5da7433412e2776d3a0e049dc
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-07-16 05:43:11 +0800

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,18 +9,18 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-  void *ret;
-  char *ptr;
-  unsigned int i;
+	void *ret;
+	char *ptr;
+	unsigned int i;
 
-  if (nmemb == 0 || size == 0)
-    return (0);
+	if (nmemb == 0 || size == 0)
+		return (0);
 
-  ret = malloc(nmemb * size);
-  if (ret == 0)
-    return (0);
+	ret = malloc(nmemb * size);
+	if (ret == 0)
+		return (0);
 
-  for (i = nmemb * size, ptr = ret; i; i--)
-    *ptr++ = 0;
-  return (ret);
+	for (i = nmemb * size, ptr = ret; i; i--)
+		*ptr++ = 0;
+	return (ret);
 }
