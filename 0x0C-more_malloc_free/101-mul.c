0aef56d33cd19d666fbe2b47eb583792a3814d5a
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-07-16 05:42:15 +0800

    101-mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -8,8 +8,8 @@
  */
 void _prntstr(char *s)
 {
-  while (*s)
-    _putchar(*s++);
+	while (*s)
+		_putchar(*s++);
 }
 
 /**
@@ -22,26 +22,26 @@ void _prntstr(char *s)
  */
 long int numstrchk(char *s)
 {
-  long int len = 0;
+	long int len = 0;
 
-  if (*s == 0)
-    {
-      _prntstr("Error\n");
-      exit(98);
-    }
-
-  while (*s)
-    {
-      if (*s < '0' || *s > '9')
+	if (*s == 0)
 	{
-	  _prntstr("Error\n");
-	  exit(98);
+		_prntstr("Error\n");
+		exit(98);
 	}
-      *s -= '0';
-      len++;
-      s++;
-    }
-  return (len);
+
+	while (*s)
+	{
+		if (*s < '0' || *s > '9')
+		{
+			_prntstr("Error\n");
+			exit(98);
+		}
+		*s -= '0';
+		len++;
+		s++;
+	}
+	return (len);
 }
 
 /**
@@ -54,22 +54,22 @@ long int numstrchk(char *s)
  */
 void *_calloc_buffer(long int num, long int size)
 {
-  void *ret;
-  char *ptr;
+	void *ret;
+	char *ptr;
 
-  ret = malloc(num * size);
-  if (ret == 0)
-    {
-      exit(98);
-    }
+	ret = malloc(num * size);
+	if (ret == 0)
+	{
+		exit(98);
+	}
 
-  size = size * num;
-  ptr = ret;
-  ptr[--size] = 0;
-  while (size--)
-    ptr[size] = '0';
+	size = size * num;
+	ptr = ret;
+	ptr[--size] = 0;
+	while (size--)
+		ptr[size] = '0';
 
-  return (ret);
+	return (ret);
 }
 
 /**
@@ -82,12 +82,12 @@ void *_calloc_buffer(long int num, long int size)
  */
 char *trimzero(char *s)
 {
-  while (*s == '0')
-    if (*(s + 1) != 0)
-      s++;
-    else
-      break;
-  return (s);
+	while (*s == '0')
+		if (*(s + 1) != 0)
+			s++;
+		else
+			break;
+	return (s);
 }
 
 /**
@@ -100,44 +100,44 @@ char *trimzero(char *s)
  */
 int main(int ac, char **av)
 {
-  long int len1, len2, lenres, i, j;
-  char *res;
+	long int len1, len2, lenres, i, j;
+	char *res;
 
-  if (ac != 3)
-    {
-      _prntstr("Error\n");
-      return (98);
-    }
-  av[2] = trimzero(av[2]);
-  av[1] = trimzero(av[1]);
-  if (*av[1] == '0' || *av[2] == '0')
-    {
-      _prntstr("0\n");
-      return (0);
-    }
-  len1 = numstrchk(av[1]);
-  len2 = numstrchk(av[2]);
-  lenres = len1 + len2;
-  res = _calloc_buffer(lenres + 1, sizeof(char));
+	if (ac != 3)
+	{
+		_prntstr("Error\n");
+		return (98);
+	}
+	av[2] = trimzero(av[2]);
+	av[1] = trimzero(av[1]);
+	if (*av[1] == '0' || *av[2] == '0')
+	{
+		_prntstr("0\n");
+		return (0);
+	}
+	len1 = numstrchk(av[1]);
+	len2 = numstrchk(av[2]);
+	lenres = len1 + len2;
+	res = _calloc_buffer(lenres + 1, sizeof(char));
 
-  for (i = lenres - 1, len1--; len1 >= 0; len1--, i += len2 - 1)
-    for (j = len2 - 1; j >= 0; j--, i--)
-      {
-	res[i] = (av[1][len1] * av[2][j] % 10) + res[i];
-	res[i - 1] = (av[1][len1] * av[2][j] / 10) + res[i - 1];
-	if (res[i] > '9')
-	  {
-	    res[i] -= 10;
-	    res[i - 1]++;
-	  }
-      }
+	for (i = lenres - 1, len1--; len1 >= 0; len1--, i += len2 - 1)
+		for (j = len2 - 1; j >= 0; j--, i--)
+		{
+			res[i] = (av[1][len1] * av[2][j] % 10) + res[i];
+			res[i - 1] = (av[1][len1] * av[2][j] / 10) + res[i - 1];
+			if (res[i] > '9')
+			{
+				res[i] -= 10;
+				res[i - 1]++;
+			}
+		}
 
-  if (*res == '0')
-    _prntstr(res + 1);
-  else
-    _prntstr(res);
-  _putchar('\n');
-  free(res);
+	if (*res == '0')
+		_prntstr(res + 1);
+	else
+		_prntstr(res);
+	_putchar('\n');
+	free(res);
 
-  return (0);
+	return (0);
 }
