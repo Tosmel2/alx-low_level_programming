e4fea4c5c1c455e1ba3db1e20817fcadcce506e3
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-07-16 05:33:25 +0800

    string concat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -13,34 +13,34 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-  unsigned int len1, len2, i;
-  char *ptr, *ret;
+	unsigned int len1, len2, i;
+	char *ptr, *ret;
 
-  if (!s1)
-    s1 = "";
-  if (!s2)
-    s2 = "";
+	if (!s1)
+		s1 = "";
+	if (!s2)
+		s2 = "";
 
-  for (len1 = 0, ptr = s1; *ptr; ptr++)
-    len1++;
-  for (len2 = 0, ptr = s2; *ptr; ptr++)
-    len2++;
-  if (n > len2)
-    n = len2;
+	for (len1 = 0, ptr = s1; *ptr; ptr++)
+		len1++;
+	for (len2 = 0, ptr = s2; *ptr; ptr++)
+		len2++;
+	if (n > len2)
+		n = len2;
 
-  ret = malloc((len1 + n + 1) * sizeof(char));
-  if (!ret)
-    return (0);
+	ret = malloc((len1 + n + 1) * sizeof(char));
+	if (!ret)
+		return (0);
 
-  ptr = ret;
-  while (*s1)
-    *ptr++ = *s1++;
-  i = 0;
-  while (i < n)
-    {
-      *ptr++ = s2[i++];
-    }
-  *ptr = 0;
+	ptr = ret;
+	while (*s1)
+		*ptr++ = *s1++;
+	i = 0;
+	while (i < n)
+	{
+		*ptr++ = s2[i++];
+	}
+	*ptr = 0;
 
-  return (ret);
+	return (ret);
 }
