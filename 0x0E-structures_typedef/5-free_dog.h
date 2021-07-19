445ca135b50ee727fd4383458d7f43e5a7d46658
tosmel2 <adewaletosin0808@gmail.com>
2021-07-19 04:19:45 -0700

    5-free dog

---
+++ 0x0E-structures_typedef/5-free_dog.h
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - frees a dog
+ *
+ * @d: dog to free
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+  if (d == 0)
+    return;
+  if (d->name)
+    free(d->name);
+  if (d->owner)
+    free(d->owner);
+  free(d);
+}
