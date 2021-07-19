e0967c03cacb2e2541b4d51bb51bf9728515838b
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-07-19 20:12:45 +0800

    free dog

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -9,11 +9,11 @@
  */
 void free_dog(dog_t *d)
 {
-  if (d == 0)
-    return;
-  if (d->name)
-    free(d->name);
-  if (d->owner)
-    free(d->owner);
-  free(d);
+	if (d == 0)
+		return;
+	if (d->name)
+		free(d->name);
+	if (d->owner)
+		free(d->owner);
+	free(d);
 }
