abef87d6af543a7f37890a8295617431f8e5a4be
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-07-20 00:09:09 +0800

    Update 2-main.c

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /**
- * main - print name of file main was compiled from
- *
- * Return: 0 on success
- */
+* main - print name of file main was compiled from
+*
+* Return: 0 on success
+*/
 int main(void)
 {
   printf(__FILE__"\n");
