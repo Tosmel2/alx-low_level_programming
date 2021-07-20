1b1a8119150cdbb71ebde7cdd89deae8f864ab35
tosmel2 <adewaletosin0808@gmail.com>
2021-07-20 09:36:17 -0700

    dunctions

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *  * print_name - prints name using function f
+ *   *
+ *    * @name: name to print
+ *     * @f: function to use
+ *      *
+ *       * Return: void
+ *        */
+void print_name(char *name, void (*f)(char *))
+{
+		if (f != NULL && name != NULL)
+					(*f)(name);
+}
