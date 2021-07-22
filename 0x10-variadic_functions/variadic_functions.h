3a540ceafd7ff2dc071ea8287e7b6eb8435ccda6
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-07-22 19:25:15 +0800

    variadic functions h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,1 +1,25 @@
-#heaterccdnns
+#ifndef VARIADICFUNCTIONSH
+#define VARIADICFUNCTIONSH
+#include <stdarg.h>
+
+int sum_them_all(const unsigned int n, ...);
+void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
+void print_all(const char * const format, ...);
+
+int _putchar(char c);
+
+/**
+ * struct funckey - a key for picking functions to use for printing
+ * in 3-print_all.c
+ *
+ * @f: function to use for printing
+ * @spec: character specifier
+ */
+typedef struct funckey
+{
+	void (*f)(va_list);
+	char spec;
+} funckey;
+
+#endif
