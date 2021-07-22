fb9eedf873d72ea717903f18636d4a690eed0dbf
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-07-22 08:24:56 +0800

    3-op_functions

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,64 +1,74 @@
+#include "3-calc.h"
+
 /**
- * op_add - adds two numbers
- *
- * @a: one integer
- * @b: second integer
- *
- * Return: sum of integers
+ * op_add - return the suma of a and b
+ * @a: int type Integer
+ * @b: int type Intger
+ * Return: add
  */
+
+
 int op_add(int a, int b)
 {
 	return (a + b);
 }
 
 /**
- * op_sub - subtract two numbers
- *
- * @a: one integer
- * @b: second integer
- *
- * Return: a - b
+ * op_sub - return the difference of a and b
+ * @a: int type Integer
+ * @b: int type Intger
+ * Return: sub
  */
+
 int op_sub(int a, int b)
 {
 	return (a - b);
 }
 
 /**
- * op_mul - multiplies two numbers
- *
- * @a: one integer
- * @b: second integer
- *
- * Return: a * b
+ * op_mul - return the product of a and b
+ * @a: int type Integer
+ * @b: int type Intger
+ * Return: mul
  */
+
 int op_mul(int a, int b)
 {
-	return (a * b);
+	int mul = a * b;
+
+	return (mul);
 }
 
 /**
- * op_div - divides two numbers
- *
- * @a: one integer
- * @b: second integer
- *
- * Return: a / b
+ * op_div - return the division of a and b
+ * @a: int type Integer
+ * @b: int type Intger
+ * Return: div
  */
+
 int op_div(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a / b);
 }
 
 /**
- * op_mod - modulus two numbers
- *
- * @a: one integer
- * @b: second integer
- *
- * Return: a % b
+ * op_mod - return the remainder of the division of a and b
+ * @a: int type Integer
+ * @b: int type Intger
+ * Return: mod
  */
+
 int op_mod(int a, int b)
 {
+	if (b == 0)
+	{
+		printf("Error\n");
+		exit(100);
+	}
 	return (a % b);
 }
