9ce2eb2166869c298a5e3ab633af0f4aa88cdbfa
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-07-22 19:22:40 +0800

    hello_holberton

--- 0x10-variadic_functions/100-hello_holberton.asm
+++ 0x10-variadic_functions/100-hello_holberton.asm
@@ -1,2 +1,15 @@
-#lets gbdrjbd js
-bshhsb
+    section .data
+msg:	db	"Hello, Holberton",10
+    global main
+
+    section .text
+main:
+    mov	rax, 1
+    mov	rdi, 1
+    mov	rsi, msg
+    mov	rdx, 17
+    syscall
+
+    mov rax, 60
+    xor rdi, rdi
+    syscall
