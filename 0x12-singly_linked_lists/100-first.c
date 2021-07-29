dfa46044747129cbfab0d98d3245a9dd8a6acb4f
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-07-29 23:05:29 +0800

    function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,1 +1,11 @@
-gsgheb
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * turtle - prints before main
+ */
+void __attribute__ ((constructor)) turtle()
+{
+	printf("You're beat! and yet, you must allow,\n"
+	       "I bore my house upon my back!\n");
+}
