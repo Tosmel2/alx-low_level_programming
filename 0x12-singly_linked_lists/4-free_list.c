1d59e98da539de6608a4dea7c0309011582f4fc6
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-07-29 23:11:42 +0800

    function that frees a list_t list.

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,1 +1,28 @@
-thdvbs
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * free_list - free a list_t list
+ *
+ * @head: head of list to free
+ *
+ * Return: void
+ */
+void free_list(list_t *head)
+{
+	list_t *next;
+
+	if (head == NULL)
+		return;
+	next = head->next;
+
+	while (head != NULL)
+	{
+		if (head->str != NULL)
+			free(head->str);
+		free(head);
+		head = next;
+		if (head != NULL)
+			next = head->next;
+	}
+}
