0dad5ad7fcca17becab57bebfbb21387f0b101a0
Tosmel2 <84603888+Tosmel2@users.noreply.github.com>
2021-09-25 07:06:50 +0800

    dynamic.c

--- 0x18-dynamic_libraries/dynamic.c
+++ 0x18-dynamic_libraries/dynamic.c
@@ -1,176 +1,176 @@
 /**
- *  * _putchar - prints char
- *   * @c: char to print
- *    * Return: always 1
- *     */
+ * _putchar - prints char
+ * @c: char to print
+ * Return: always 1
+ */
 
 int _putchar(char c)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * _islower - prints char
- *   * @c: char to print
- *    * Return: always 1
- *     */
+ * _islower - prints char
+ * @c: char to print
+ * Return: always 1
+ */
 
 int _islower(int c)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * _isalpha - prints char
- *   * @c: char to print
- *    * Return: always 1
- *     */
+ * _isalpha - prints char
+ * @c: char to print
+ * Return: always 1
+ */
 
 int _isalpha(int c)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * _abs - prints char
- *   * @n: char to print
- *    * Return: always 1
- *     */
+ * _abs - prints char
+ * @n: char to print
+ * Return: always 1
+ */
 
 int _abs(int n)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * _isupper - prints char
- *   * @c: char to print
- *    * Return: always 1
- *     */
+ * _isupper - prints char
+ * @c: char to print
+ * Return: always 1
+ */
 
 int _isupper(int c)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * _isdigit - prints char
- *   * @c: char to print
- *    * Return: always 1
- *     */
+ * _isdigit - prints char
+ * @c: char to print
+ * Return: always 1
+ */
 
 int _isdigit(int c)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * _strlen - prints char
- *   * @s: char to print
- *    * Return: always 1
- *     */
+ * _strlen - prints char
+ * @s: char to print
+ * Return: always 1
+ */
 
 int _strlen(char *s)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * _puts - prints char
- *   * @s: char to print
- *    * Return: always 1
- *     */
+ * _puts - prints char
+ * @s: char to print
+ * Return: always 1
+ */
 
 void _puts(char *s)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * *_strcpy - prints char
- *   * @dest: char to print
- *    * @src: char to print
- *     * Return: always 1
- *      */
+ * *_strcpy - prints char
+ * @dest: char to print
+ * @src: char to print
+ * Return: always 1
+ */
 
 char *_strcpy(char *dest, char *src)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * _atoi - prints char
- *   * @s: char to print
- *    * Return: always 1
- *     */
+ * _atoi - prints char
+ * @s: char to print
+ * Return: always 1
+ */
 
 int _atoi(char *s)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * *_strcat - prints char
- *   * @dest: char to print
- *    * @src: char to print
- *     * Return: always 1
- *      */
+ * *_strcat - prints char
+ * @dest: char to print
+ * @src: char to print
+ * Return: always 1
+ */
 
 char *_strcat(char *dest, char *src)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * *_strncat - prints char
- *   * @dest: char to print
- *    * @src: char to print
- *     * @n: blabla
- *      * Return: always 1
- *       */
+ * *_strncat - prints char
+ * @dest: char to print
+ * @src: char to print
+ * @n: blabla
+ * Return: always 1
+ */
 
 char *_strncat(char *dest, char *src, int n)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * *_strncpy - prints char
- *   * @dest: char to print
- *    * @src: char to print
- *     * @n: something
- *      * Return: always 1
- *       */
+ * *_strncpy - prints char
+ * @dest: char to print
+ * @src: char to print
+ * @n: something
+ * Return: always 1
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * _strcmp - prints char
- *   * @s1: char to print
- *    * @s2: something
- *     * Return: always 1
- *      */
+ * _strcmp - prints char
+ * @s1: char to print
+ * @s2: something
+ * Return: always 1
+ */
 
 int _strcmp(char *s1, char *s2)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * *_memset - prints char
- *   * @s: char to print
- *    * @b: char to print
- *     * @n: something
- *      * Return: always 1
- *       */
+ * *_memset - prints char
+ * @s: char to print
+ * @b: char to print
+ * @n: something
+ * Return: always 1
+ */
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	return (1);
+  return (1);
 }
 /**
- *  * *_memcpy - prints char
- *   * @dest: char to print
- *    * @src: char to print
- *     * @n: something
- *      * Return: always 1
- *       */
+ * *_memcpy - prints char
+ * @dest: char to print
+ * @src: char to print
+ * @n: something
+ * Return: always 1
+ */
 
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	return (1);
+  return (1);
 }
 /**
- *_strchr - prints char
+ * *_strchr - prints char
  * @s: char to print
  * @c: char to print
  * Return: always 1
@@ -178,7 +178,7 @@ char *_memcpy(char *dest, char *src, unsigned int n)
 
 char *_strchr(char *s, char c)
 {
-	return (1);
+  return (1);
 }
 /**
  * _strspn - prints char
@@ -189,10 +189,10 @@ char *_strchr(char *s, char c)
 
 unsigned int _strspn(char *s, char *accept)
 {
-	return (1);
+  return (1);
 }
 /**
- *_strpbrk - prints char
+ * *_strpbrk - prints char
  * @s: char to print
  * @accept: char to print
  * Return: always 1
@@ -200,7 +200,7 @@ unsigned int _strspn(char *s, char *accept)
 
 char *_strpbrk(char *s, char *accept)
 {
-	return (1);
+  return (1);
 }
 /**
  * *_strstr - prints char
@@ -211,6 +211,5 @@ char *_strpbrk(char *s, char *accept)
 
 char *_strstr(char *haystack, char *needle)
 {
-	return (1);
+  return (1);
 }
-
