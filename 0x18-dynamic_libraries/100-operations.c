ba4e638babb778f09c82f0cae1ab5352a88bf271
Tosmel2 <Adewaletosin0808@gmail.com>
2021-09-24 16:14:25 -0700

    dynamic libraries

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * *add - adds 2 numbers
+ * *sub - subtract 2 numbers
+ * *mul - multiply 2 numbers
+ * *div - divides 2 numbers
+ * *mod - divides 2 numbers and returns its remainder
+ * *@a: int type number
+ * *@b: int type number
+ * *Return : results of calculations
+ * */
+int add(int a, int b)
+{
+	return (a + b);
+}
+int sub(int a, int b)
+{
+	return (a - b);
+}
+int mul(int a, int b)
+{
+	return (a * b);
+}
+int  div(int  a, int b)
+{
+	return (a / b);
+}
+int mod(int a, int b)
+{
+	return (a % b);
+}
+
